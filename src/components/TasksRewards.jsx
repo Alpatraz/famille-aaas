324a57b915da112510b7a5daad14e1339ed7262f
Alpatraz <guillaume.se@gmail.com>
2025-05-23 15:24:42 -0400

    Update Dashboard to pass userId to ChildTasks

--- src/components/TasksRewards.jsx
+++ src/components/TasksRewards.jsx
@@ -3,7 +3,7 @@ import ChildTasks from './ChildTasks'
 export default function TasksRewards({ user }) {
   return (
     <div>
-      <ChildTasks name={user.displayName} />
+      <ChildTasks name={user.displayName} userId={user.id} />
     </div>
   )
 }
