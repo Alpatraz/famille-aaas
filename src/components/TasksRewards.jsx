836ab44b7dd7b72ec6e2ddc23f8a491517176431
Alpatraz <guillaume.se@gmail.com>
2025-05-23 17:24:00 -0400

    Correction du chargement des tâches

--- src/components/TasksRewards.jsx
+++ src/components/TasksRewards.jsx
@@ -1,9 +1,13 @@
 import ChildTasks from './ChildTasks'
 
 export default function TasksRewards({ user }) {
+  if (!user || !user.uid) {
+    return <p>Chargement des données utilisateur...</p>
+  }
+
   return (
     <div>
-      <ChildTasks name={user.displayName} userId={user.id} />
+      <ChildTasks name={user.displayName} userId={user.uid} />
     </div>
   )
 }
